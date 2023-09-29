2bd93e79e751d9ab50be7df6b0d4ae34d23c9e3d
Victor Sun <vtsun@email.unc.edu>
2023-09-29 13:58:48 -0400

    Convert website to angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { StickyHeaderComponent } from './sticky-header/sticky-header.component';
-import { PageBannerComponent } from './page-banner/page-banner.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { CareersComponent } from './careers/careers.component';
+import { DataComponent } from './data/data.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     StickyHeaderComponent,
-    PageBannerComponent
+    ResourcesComponent,
+    CareersComponent,
+    DataComponent,
+    ProjectComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'resources', component: ResourcesComponent},
+      { path: 'careers', component: CareersComponent},
+      { path: 'data', component: DataComponent},
+      { path: 'project', component: ProjectComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
