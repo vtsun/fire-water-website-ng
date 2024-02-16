37f5bf539a17d846868b35e467daf496316c1610
Victor Sun <vtsun@email.unc.edu>
2024-02-16 12:56:45 -0500

    Add content to project overview pg. Add scrolling links in sidenav

--- src/app/project-overview/project-overview.component.ts
+++ src/app/project-overview/project-overview.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-project-overview',
   templateUrl: './project-overview.component.html',
   styleUrls: ['./project-overview.component.css']
 })
-export class ProjectOverviewComponent {
+export class ProjectOverviewComponent implements OnInit {
+  constructor(private route: ActivatedRoute) { }
 
+  ngOnInit() {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment !== null) {
+        this.scrollToElement(fragment);
+      }
+    });
+  }
+
+  scrollToElement(elementId: string) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const elementPosition = element.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.scrollY - 100;
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
