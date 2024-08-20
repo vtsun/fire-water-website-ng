e7a21bf232df4a14489fb97bffda2eb2a0f65632
Victor Sun <vtsun@email.unc.edu>
2024-08-20 10:18:31 -0400

    Update card to use components

--- src/app/home-page-card/home-page-card.component.ts
+++ src/app/home-page-card/home-page-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home-page-card',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './home-page-card.component.css'
 })
 export class HomePageCardComponent {
+  @Input() imageSrc: string = '';
+  @Input() buttonText: string = '';
+  @Input() link: string = '/'
 
 }
