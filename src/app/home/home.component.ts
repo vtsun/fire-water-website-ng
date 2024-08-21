345d199571fda31328d64f08eedbf6d16d927544
Victor Sun <vtsun@email.unc.edu>
2024-08-21 15:12:10 -0400

    Add scroll arrow on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  @ViewChild('scrollContainer', { static: true }) scrollContainer?: ElementRef;
+
+  scrollRight() {
+    const container = this.scrollContainer?.nativeElement;
+    const cardWidth = container.querySelector('app-home-page-card').offsetWidth;
+    container.scrollBy({ left: cardWidth + 48, behavior: 'smooth' });
+  }
 
 }
